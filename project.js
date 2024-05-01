a1d911e23ae36d865431866da16fc2e04d7a5cc3
DanTheEpicMan <98715915+DanTheEpicMan@users.noreply.github.com>
2024-05-01 19:00:35 -0400

    sharing to desktop
    I need to share the code to my desktop and work on it from their so Im just going to commit it.

---
+++ project.js
@@ -0,0 +1,11 @@
+function onPageLoad() {
+    var timelineContainer = document.getElementById('timeline');
+    // Use timelineContainer to access and manipulate the container
+    
+    timelineContainer.style.backgroundColor = 'white';
+    timelineContainer.style.height = '100%';
+    timelineContainer.style.width = '1px';
+    timelineContainer.style.backgroundColor = 'white';
+}
+
+window.addEventListener('load', onPageLoad);
