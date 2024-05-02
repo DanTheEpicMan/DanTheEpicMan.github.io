b83219419e151f2a7b2665a068b551ecba0fb90d
DanTheEpicMan <98715915+DanTheEpicMan@users.noreply.github.com>
2024-05-02 17:32:49 -0400

    stated working on the projects page
    got inspiration from a website about how I could better structure the timeline too look good on a website and started working on it mainly in JS as the animations seem more complex than whatever can be done with normal html/css

--- project.js
+++ project.js
@@ -1,11 +1,44 @@
-function onPageLoad() {
-    var timelineContainer = document.getElementById('timeline');
-    // Use timelineContainer to access and manipulate the container
+//modaled after: https://webflow.com/made-in-webflow/website/relume-timeline-cloneable
+function OnResize() {
+    console.clear();
+    //Get container width of SmallWidthTimeline
+    const TempElement = document.getElementById("SmallWidthTimeline");
+    var width = TempElement.offsetWidth;
+    if (width == 0) {
+        const TempElement = document.getElementById("BigWidthTimeline");
+        width = TempElement.offsetWidth;
+    }
+
+    //Hides none active timeline and sets var timeline to active one
+    if (width > 768) { //Big
+        document.getElementById("SmallWidthTimeline").style.display = "none";
+        document.getElementById("BigWidthTimeline").style.display = "block";
+        var timeline = document.getElementById("BigWidthTimeline");
+
+
+
+
+
+
+    } else { // Samll
+        document.getElementById("BigWidthTimeline").style.display = "none";
+        document.getElementById("SmallWidthTimeline").style.display = "block";
+        var timeline = document.getElementById("SmallWidthTimeline");
+
+        //try to expand on this, maybe search up make line JS
+        timeline.style.borderLeft = "1px solid white";
+
+
+
+
+        
+    }
+
     
-    timelineContainer.style.backgroundColor = 'white';
-    timelineContainer.style.height = '100%';
-    timelineContainer.style.width = '1px';
-    timelineContainer.style.backgroundColor = 'white';
+    
+
+
 }
 
-window.addEventListener('load', onPageLoad);
+window.addEventListener('load', OnResize);
+window.addEventListener('resize', OnResize);
