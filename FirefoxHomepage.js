2a4d11b487935a2fd63a468362b37ce6f4bcba1c
DanTheEpicMan <98715915+DanTheEpicMan@users.noreply.github.com>
2024-06-19 19:40:29 -0400

    made search bar minimalist

--- FirefoxHomepage.js
+++ FirefoxHomepage.js
@@ -1,15 +1,21 @@
 function getPercent() {
-    EndDate = 1725616800; // Sep 6 6:00 AM 2024
-    TotalTime  = 6825600; // From Jun 19 6:00 AM 2024 to Sep 6 6:00 AM 2024
-    NowTime = Math.round(new Date().getTime() / 1000); //Now
+    EndDate = 1725616800000; // Sep 6 6:00 AM 2024
+    TotalTime  = 6825600000; // From Jun 19 6:00 AM 2024 to Sep 6 6:00 AM 2024
+    NowTime = new Date().getTime(); //Now
     TimeLeft = EndDate - NowTime;
     Percent = (TimeLeft / TotalTime) * 100;
-    return Percent.toFixed(4);
+    return Percent.toFixed(6);
 }
 
 function formatSearch(search) {
     baseText = "https://www.google.com/search?client=firefox-b-1-d&q=";
     finalString = baseText + search.replace(" ", "+");
+    if (search.slice(-5).includes('.') && !search.includes(' ')) {
+        finalString = search;
+        if (!search.startsWith("http")) {
+            finalString = "https://" + finalString;
+        }
+    }
     console.log(finalString);
     window.location.href = finalString;
 }
